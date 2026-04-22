b3f9b19fee10777a63a56719b305f6a41588c07c
jhemesparma1-ux <jhemesparma1@gmail.com>
2026-04-21 21:47:55 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,8 +1,16 @@
 export default async function handler(req, res) {
   const url = 'https://script.google.com/macros/s/AKfycbxIRCSzxoQOV697P_tA8gviAXKDnWmbi_BKjqKNvl9ds7-R2JFpSeY1u-AOIoebDlZP/exec';
+  
   try {
-    const resp = await fetch(url, { redirect: 'follow' });
+    const controller = new AbortController();
+    setTimeout(() => controller.abort(), 10000);
+    
+    const resp = await fetch(url, { 
+      redirect: 'follow',
+      signal: controller.signal
+    });
     const text = await resp.text();
+    
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'application/json');
     res.status(200).send(text);
