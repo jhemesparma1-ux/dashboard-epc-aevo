1ea3e25fe231d6e95b63203e764ba1e8f5b081e1
jhemesparma1-ux <jhemesparma1@gmail.com>
2026-04-22 10:25:28 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,16 +1,9 @@
 export default async function handler(req, res) {
-  const url = 'https://script.google.com/macros/s/AKfycbxIRCSzxoQOV697P_tA8gviAXKDnWmbi_BKjqKNvl9ds7-R2JFpSeY1u-AOIoebDlZP/exec';
-  
+  const obra = req.query.obra || 'planura';
+  const url = 'https://script.google.com/macros/s/AKfycbxIRCSzxoQOV697P_tA8gviAXKDnWmbi_BKjqKNvl9ds7-R2JFpSeY1u-AOIoebDlZP/exec?obra=' + obra;
   try {
-    const controller = new AbortController();
-    setTimeout(() => controller.abort(), 10000);
-    
-    const resp = await fetch(url, { 
-      redirect: 'follow',
-      signal: controller.signal
-    });
+    const resp = await fetch(url, { redirect: 'follow' });
     const text = await resp.text();
-    
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'application/json');
     res.status(200).send(text);
