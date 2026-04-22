366636b29f3a7317365e95f80435f2ba3a2a3935
jhemesparma1-ux <jhemesparma1@gmail.com>
2026-04-21 21:25:16 -0300

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  const url = 'https://script.google.com/macros/s/AKfycbxIRCSzxoQOV697P_tA8gviAXKDnWmbi_BKjqKNvl9ds7-R2JFpSeY1u-AOIoebDlZP/exec';
+  try {
+    const resp = await fetch(url, { redirect: 'follow' });
+    const text = await resp.text();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send(text);
+  } catch(e) {
+    res.status(500).json({ error: e.message });
+  }
+}
